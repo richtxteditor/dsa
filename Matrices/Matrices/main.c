65737fc172c5500d2db35b9653f0ba4493e655f7
John <johnrichardmolina@gmail.com>
2026-03-27 16:46:45 -0400

    Implement lower triangular matrix operations and expand C matrix project
    - Add row-major, column-major, and diagonal display for LTMatrix in C++
    - Expand C matrices with upper triangular and symmetric implementations
    - Add menu-driven test programs for both C and C++ matrix projects

--- Matrices/Matrices/main.c
+++ Matrices/Matrices/main.c
@@ -6,24 +6,24 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Matrix
 {
-    int A[10];
+    int *A;
     int n;
 };
 
 void Set(struct Matrix *m, int i, int j, int x)
 {
-    if (i==j)
-        m->A[i-1] = x;
-        
+    if (i>=j)
+        m->A[m->n*(j-1)+(j-2)*(j-1)/2+i-j] = x;
 }
 
 int Get(struct Matrix m, int i, int j)
 {
-    if (i==j)
-        return m.A[i-1];
+    if (i>=j)
+        return m.A[m.n*(j-1)+(j-2)*(j-1)/2+i-j];
     else
         return 0;
 }
@@ -31,12 +31,12 @@ int Get(struct Matrix m, int i, int j)
 void Display(struct Matrix m)
 {
     int i, j;
-    for(i = 0; i < m.n; i++)
+    for(i = 1; i <= m.n; i++)
     {
-        for(j = 0; j < m.n; j++)
+        for(j = 1; j <= m.n; j++)
         {
-            if (i == j)
-                printf("%d ", m.A[i-1]);
+            if (i >= j)
+                printf("%d ", m.A[m.n*(j-1)+(j-2)*(j-1)/2+i-j]);
             else
                 printf("0 ");
         }
@@ -44,20 +44,26 @@ void Display(struct Matrix m)
     }
 }
 
-
-
 int main(void)
 {
     struct Matrix m;
-    m.n = 4;
+    int i, j, x;
     
-    Set(&m,1,1,5);
-    Set(&m,2,2,8);
-    Set(&m,3,3,9);
-    Set(&m,4,4,12);
+    printf("Enter the dimensions: ");
+    scanf("%d", &m.n);
+    // column-major formula
+    m.A = (int *)malloc(m.n*(m.n-1)/2*sizeof(int));
     
-    printf("%d \n", Get(m,3,3));
+    printf("Enter all elemens in the matrix below\n");
+    
+    for (i = 1; i <= m.n; i++) {
+        for (j = 1; j <= m.n; j++) {
+            scanf("%d", &x);
+            Set(&m, i, j, x);
+            
+        }
+    }
+    printf("\n\n");
     Display(m);
-    
     return 0;
 }
--- MatricesCPP/MatricesCPP/main.cpp
+++ MatricesCPP/MatricesCPP/main.cpp
@@ -5,10 +5,92 @@
 //  Created by richie on 12/8/25.
 //
 
+// Upper Triangular Matrix
+// M[i,j] = if i > j
+// M[i,j] = non-zero if i<=j
+// non-zero = 5+4+3+2+1
+//          = n + n 01 + ... + 3 + 2 + 1 = n(n + 1)/2
+// zero = n(n - 1)/ 2
+
+
+
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+class LowerTriangle
+{
+private:
+    int *A;
+    int n;
+public:
+    LowerTriangle()
+    {
+        n = 2;
+        A = new int[2 * (2 + 1) / 2];
+    }
+    LowerTriangle(int n)
+    {
+        this->n = n;
+        A = new int[n * (n + 1) / 2];
+    }
+    ~LowerTriangle()
+    {
+        delete []A;
+    }
+    
+    void Set(int i, int j, int x);
+    int Get(int i, int j);
+    void Display();
+    int GetDimension() {return n;}
+};
+
+void LowerTriangle::Set(int i, int j, int x)
+{
+    // non-zeroes will only be present when i >= j
+    if(i >= j)
+        A[i * (i - 1) / 2 + j - 1] = x;
+}
+
+int LowerTriangle::Get(int i, int j)
+{
+    if (i >= j)
+        return A[i * (i - 1) / 2 + j - 1];
+    return 0;
+}
+
+void LowerTriangle::Display()
+{
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= n; j++)
+        {
+            if (i >= j)
+                std::cout << A[i * (i - 1) / 2 + j - 1 ] << " ";
+            else
+                std::cout << "0 ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main()
+{
+    int d;
+    std::cout << "Enter Dimensions of the square matrix: ";
+    std::cin >> d;
+    
+    LowerTriangle lm(d);
+    
+    int x;
+    std::cout << "Enter all elements below \n";
+    
+    for(int i = 1; i <= d; i++)
+    {
+        for(int j = 1; j <= d ; j++)
+        {
+            std::cin >> x;
+            lm.Set(i,j,x);
+        }
+    }
+    lm.Display();
+    return 0;
 }
